49549584b56e7d820719d995a62a268044798d5c
Tamplier <tamplier29@mail.ru>
2020-09-05 23:42:38 +1000

    Убраны лишние строки

--- Program.cs
+++ Program.cs
@@ -55,14 +55,3 @@ namespace Diary {
 		}
 	}
 }
-
-
-//string inputString = "2000-02-02";
-//DateTime dDate;
-
-//if (DateTime.TryParse(inputString, out dDate)) {
-//    String.Format("{0:d/MM/yyyy}", dDate);
-//}
-//else {
-//    Console.WriteLine("Invalid"); // <-- Control flow goes here
-//}
